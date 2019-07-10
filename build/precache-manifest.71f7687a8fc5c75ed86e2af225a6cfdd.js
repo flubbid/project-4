1e9b619f145837eb745b26c942a92a6c91784058
flubbid <brendanwilson1@gmail.com>
2019-07-10 11:22:28 -0500

    adds express and the sets up server.js

---
+++ build/precache-manifest.71f7687a8fc5c75ed86e2af225a6cfdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6645cba7da74a8c274d654cd163fd5ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dfea8ae5fb153d156ef",
+    "url": "/static/css/main.be8288a8.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "1dfea8ae5fb153d156ef",
+    "url": "/static/js/main.2ae83226.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
