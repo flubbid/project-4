a4e96404d12d0be137ec4c5874ef2b9ddc007de7
flubbid <brendanwilson1@gmail.com>
2019-07-13 13:09:32 -0500

    adds models foldeR

---
+++ backend/models/quiz.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const quizSchema = new Schema({
+    name: { type: string, require: true },
+    description: String,
+    questions: [
+        {
+            type: Scheme.Types.ObjectID,
+            ref: 'Question'
+        }
+    ]
+
+});
+
+
+
+module.exports = mongoose.model('Quiz', quizSchema);
