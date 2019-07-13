16f8510975254c9fe94e89cf13b5d45eebf2313a
flubbid <brendanwilson1@gmail.com>
2019-07-13 14:46:44 -0500

    setups template for quizlist

---
+++ src/components/Quiz/QuizList.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class QuizList extends React.Component{
+
+    render(){
+        return (
+            <div>
+            This is the QuizList page.
+            </div>
+        )
+    }
+
+}
+
+export default QuizList;
