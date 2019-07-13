aaf40376cdbafe3081b545bc858443e1a7ee8d4e
flubbid <brendanwilson1@gmail.com>
2019-07-13 07:55:12 -0500

    adds quiz component

---
+++ src/components/Quiz/Quiz.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Quiz extends React.Component{
+
+    render(){
+        return (
+            <div>
+            Quiz Page
+            </div>
+        )
+    }
+
+}
+
+export default Quiz;
