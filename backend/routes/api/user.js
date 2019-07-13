51fa26474f7bdd76f08d35feccf686d4ef70dce5
flubbid <brendanwilson1@gmail.com>
2019-07-13 13:09:16 -0500

    updates user require filepath

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../user.model');
+const User = require('../../models/user');
 const usersCtrl = require('../../controllers/user');
 
 /*============= Public Routes ===============*/
