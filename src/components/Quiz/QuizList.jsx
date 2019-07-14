ad20b54b3004f096e232278d933991ba59d25140
flubbid <brendanwilson1@gmail.com>
2019-07-14 07:56:40 -0500

    quiz list now pulls from database and shows list of quizzes

--- src/components/Quiz/QuizList.jsx
+++ src/components/Quiz/QuizList.jsx
@@ -1,11 +1,55 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Quiz = props => (
+    <tr>
+        <td>{props.quiz.name}</td>
+        <td>{props.quiz.description}</td>
+        <td>
+            <Link to={'/edit/'+props.quiz._id}>Edit</Link>
+        </td>
+    </tr>
+)
 
 class QuizList extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {quiz: []};
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:3001/quiz/').then(response => {
+            this.setState({quiz: response.data})
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    }
+
+    quizList(){
+        return this.state.quiz.map(function(currentQuiz, i){
+            return <Quiz quiz={currentQuiz} key={i} />;
+        });
+    }
 
     render(){
         return (
             <div>
-            This is the QuizList page.
+            <h3>Quiz List:</h3>
+            <table className="table table-striped" style={{margin: 20}}>
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+            </tr>
+            </thead>
+            <tbody>
+            {this.quizList()}
+            </tbody>
+
+            </table>
             </div>
         )
     }
