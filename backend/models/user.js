3d38bf2cefed18d2b5d093881ea07cbcc2fd45b1
flubbid <brendanwilson1@gmail.com>
2019-07-15 17:41:14 -0500

    fixes typo in password that was throwing login errorS

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: {type: String},
     email: {type: String, required: true, lowercase: true, unique: true, trim: true, minlength: 3},
-    passsword: {type: String}
+    password: {type: String}
 }, {
     timestamps: true,
 });
