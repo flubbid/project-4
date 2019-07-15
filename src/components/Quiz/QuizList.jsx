142651b2ab23b343f3f5bb25d1b3ab21346ea871
flubbid <brendanwilson1@gmail.com>
2019-07-15 17:49:12 -0500

    quiz list will now display a message if no quizzes are available, delete option added

--- src/components/Quiz/QuizList.jsx
+++ src/components/Quiz/QuizList.jsx
@@ -1,62 +1,94 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
-import NavBar from '../NavBar/NavBar'
+import { getQuizzes, deleteQuiz } from '../../store/actions';
+import history from '../../utils/history';
+
+import { connect } from 'react-redux';
 
 
-const Quiz = props => (
-    <tr>
-        <td>{props.quiz.name}</td>
-        <td>{props.quiz.description}</td>
-        <td>
-            <Link to={'/edit/'+props.quiz._id}>Edit</Link>
-        </td>
-    </tr>
-)
+class QuizList extends React.Component {
 
-class QuizList extends React.Component{
-
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {quiz: []};
+        this.state = { quiz: [] };
     }
 
-    componentDidMount(){
-        axios.get('http://localhost:3001/quiz/').then(response => {
-            this.setState({quiz: response.data})
-        })
-        .catch(function(error){
-            console.log(error);
-        })
+    componentDidMount() {
+        this.props.getQuizzes();
     }
 
-    quizList(){
-        return this.state.quiz.map(function(currentQuiz, i){
-            return <Quiz quiz={currentQuiz} key={i} />;
-        });
-    }
-
-    render(){
+    render() {
         return (
             <div>
-            <NavBar user={this.props.user} handleLogout={this.props.handleLogout}/>
-            <h3>Quiz List:</h3>
-            <table className="table table-striped" style={{margin: 20}}>
-            <thead>
-            <tr>
-                <th>Name</th>
-                <th>Description</th>
-            </tr>
-            </thead>
-            <tbody>
-            {this.quizList()}
-            </tbody>
+                <h3>Quiz List:</h3>
 
-            </table>
+                {
+                    this.props.quizzes && this.props.quizzes.length
+                        ? (
+                            <table className="table table-striped" style={{ margin: 20 }}>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Description</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.props.quizzes.map((v, i) => (
+                                            <tr>
+                                                <td>{v.name}</td>
+                                                <td>{v.description}</td>
+                                                <td className='text-right'>
+                                                    <button
+                                                        onClick={(e) => {
+                                                            e.preventDefault();
+                                                            history.push(`/edit/${v._id}`, v)
+                                                        }}
+                                                        className="btn btn-sm btn-info mr-2"
+                                                    >Edit</button>
+                                                    <button
+                                                        onClick={(e) => {
+                                                            e.preventDefault();
+
+
+                                                            this.props.deleteQuiz(v._id);
+
+                                                            setTimeout(() => {
+                                                                this.props.getQuizzes();
+                                                            }, 1000);
+                                                        }}
+                                                        className="btn btn-sm btn-danger"
+                                                    >Delete</button>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+
+                            </table>
+                        )
+                        : (
+                            <div className="text-center p-5">
+                                You don't have any quizzes.
+                        </div>
+                        )
+                }
+
             </div>
         )
     }
 
 }
 
-export default QuizList;
+const mapStateToProps = (state) => {
+    console.log(state);
+
+    return {
+        user: state.user,
+        quizzes: state.quizzes,
+        error: state.quizError
+    }
+}
+
+export default connect(mapStateToProps, { getQuizzes, deleteQuiz: (data) => deleteQuiz(data) })(QuizList);
