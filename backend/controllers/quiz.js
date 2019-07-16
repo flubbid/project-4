cbcca22d5dc80294e6a5d56482626b63cac03531
flubbid <brendanwilson1@gmail.com>
2019-07-16 12:20:13 -0500

    fixes bug for quiz funct

--- backend/controllers/quiz.js
+++ backend/controllers/quiz.js
@@ -1,35 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 const Quiz = require('../models/quiz');
 const ObjectId = require('mongodb').ObjectId;
 
-const getUserFromToken = (headers) => {
-    const token = headers['authorization'];
-
-    if (!token) {
-        throw Error('You are not authorized.');
-    }
-
-    const { user } = jwt.decode(token);
-
-    if (!user || !user._id) {
-        return null;
-    }
-
-    return user;
-}
-
 router.get('/', async (req, res) => {
     try {
-        const user = getUserFromToken(req.headers);
-
-        if (!user) {
-            throw Error('You are not authorized.');
-        }
-
         const quizzes = await Quiz.find({
-            userId: new ObjectId(user._id)
+            userId: new ObjectId(req.user._id)
         })
 
         return res.json(quizzes);
@@ -40,12 +17,6 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const user = getUserFromToken(req.headers);
-
-        if (!user) {
-            throw Error('You are not authorized.');
-        }
-
         if (!req.body.name) {
             throw Error('Name is required.');
         }
@@ -54,7 +25,7 @@ router.post('/', async (req, res) => {
             throw Error('Description is required.');
         }
 
-        const quiz = await (new Quiz({ name: req.body.name, description: req.body.description, userId: new ObjectId(user._id) })).save();
+        const quiz = await (new Quiz({ name: req.body.name, description: req.body.description, userId: new ObjectId(req.user._id) })).save();
 
         return res.json(quiz);
     } catch ({ message }) {
@@ -64,12 +35,6 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
     try {
-        const user = getUserFromToken(req.headers);
-
-        if (!user) {
-            throw Error('You are not authorized.');
-        }
-
         if (!req.params.id) {
             throw Error('Quiz ID is not defined.');
         }
@@ -93,12 +58,6 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
     try {
-        const user = getUserFromToken(req.headers);
-
-        if (!user) {
-            throw Error('You are not authorized.');
-        }
-
         if (!req.params.id) {
             throw Error('Quiz ID is not defined.');
         }
